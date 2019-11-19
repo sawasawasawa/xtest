d84fc4e125bfdcabe51cb6de2f38bb7559083fba
sawasawasawa <m.e.sawka@gmail.com>
2019-11-19 20:23:50 +0400

    asdasdads
    (XSpecs) Commit on behalf of undefined (Developer)

---
+++ asda.js
@@ -0,0 +1,7 @@
+describe('Component Specification', function () {
+  describe('asddd', function () {
+    it('asdasdasd', function () {
+      throw new Error('This spec was added by xspecs and still needs to be implemented');
+    });
+  });
+});
